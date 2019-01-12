b9c6152f08b3e6327ca6a991fef69c7ec272bc9d
Jowensky Neard <jp.12@live.com>
2019-01-11 20:17:06 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,28 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require("dotenv").config();
+// process.env.JAWSDB_URL
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+ connection = mysql.createConnection({
   host: process.env.host,
   port: 3306,
   user: process.env.user,
   password: process.env.password,
   database: process.env.database 
-});
+})
+};
 
 // Make connection.
 connection.connect(function(err) {
-  // if (err) {
-  //   console.error("error connecting: " + err.stack);
-  //   return;
-  // }
-  // console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
